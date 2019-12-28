1cab32a0302b09476917e083bbffb937df036ab8
jintz <keisuke.igimi@gmail.com>
2019-12-28 10:48:16 +0900

    feat: add server start process

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('Hello Apollo server!')
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crishton'
+  }
+]
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then((config: { url: string }) => {
+  console.log(`🚀 Server ready at ${config.url}`)
+})
